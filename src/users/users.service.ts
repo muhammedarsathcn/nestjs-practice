26d6e40820c7f47d8e749fb5f21a7cf99b14efa2
muhammedarsathcn <muhammed.arsathcn@cdw.com>
2026-05-26 10:59:15 +0530

    setup: add folder structure

---
+++ src/users/users.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UsersService {
+  greetUser() {
+    const a = 10;
+    console.log(a);
+    console.log(a - a);
+    return 'Hello, world!';
+  }
+}
