d1b0ebc9ad9e169017a46d150275c8e507772e3a
muhammedarsathcn <muhammed.arsathcn@cdw.com>
2026-05-26 11:13:14 +0530

    fix: addition bug solved

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class UsersService {
   greetUser() {
-    const a = 1000;
+    const a = 10000;
     console.log(a);
     console.log(a + a + a);
     return 'Hello, world!';
